58de46b46b7a036a310198ab15d4c2bac4125d66
LPAE <daelnifsclpae@gmail.com>
2019-07-22 15:59:00 -0300

    add publisher_listener example

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/lpaeros/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="/home/lpaeros/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/lpaeros/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/lpaeros/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/lpaeros/catkin_ws/src:$ROS_PACKAGE_PATH"

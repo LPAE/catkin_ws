4757ae3151e7d9573bcf19e848f60602bd04b6a7
Lucas Seara Manoel <lucas@lucas.domain.lucas>
2019-08-03 10:31:28 -0300

    add turtlesim_arduino_lcd_keypad

---
+++ src/publisher_listener/rosserial_arduino/listener_lcd/src/pinout.h
@@ -0,0 +1,16 @@
+#ifndef PINOUT_H_
+#define PINOUT_H_
+
+#define STEP_X A0
+#define DIR_X A1
+#define ENDSTOP_X 42
+
+#define STEP_Y A6
+#define DIR_Y A7
+#define ENDSTOP_Y 40
+
+#define STEP_Z 46
+#define DIR_Z 48
+#define ENDSTOP_Z 38
+
+#endif
--- src/turtlesim_arduino_lcd_keypad/src/main.cpp
+++ src/turtlesim_arduino_lcd_keypad/src/main.cpp
@@ -1,37 +1,33 @@
 #define USE_USBCON
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include "turtlesim/Pose.h"
 #include <std_msgs/UInt16.h>
 #include <Stepper.h>
-#include "pinout.h"
 
 // -----------------------------------------------------------------------------
 ros::NodeHandle nh;
 
-int key_signal;
+// -----------------------------------------------------------------------------
+turtlesim::Pose msg_pose;
 
-int ref_alpha_1=0, ref_alpha_2=0, ref_teta=200;
-int alpha_1=0, alpha_2=0, teta=0;
+float_t x;
+float_t y;
+float_t theta;
+float_t linear_velocity;
+float_t angular_velocity;
 
-void alpha_1_callback(const std_msgs::UInt16& angle)
+void msg_pose_callback(const turtlesim::Pose &msg_pose)
 {
-    ref_alpha_1 = angle.data;
+    x = msg_pose.x;
+    y = msg_pose.y;
+    theta = msg_pose.theta;
+    linear_velocity = msg_pose.linear_velocity;
+    angular_velocity = msg_pose.angular_velocity;
 }
+ros::Subscriber<turtlesim::Pose> sub_msg_pose("/turtle1/pose", &msg_pose_callback);
 
-void alpha_2_callback(const std_msgs::UInt16& angle)
-{
-    ref_alpha_2 = angle.data;
-}
-
-void teta_callback(const std_msgs::UInt16& angle)
-{
-    ref_teta = angle.data;
-}
-
-ros::Subscriber<std_msgs::UInt16> sub_alpha_1("alpha_1", alpha_1_callback);
-ros::Subscriber<std_msgs::UInt16> sub_alpha_2("alpha_2", alpha_2_callback);
-ros::Subscriber<std_msgs::UInt16> sub_teta("teta", teta_callback);
-
+// -----------------------------------------------------------------------------
 geometry_msgs::Twist cmd_vel_msg;
 ros::Publisher pub_cmd_vel("/turtle1/cmd_vel", &cmd_vel_msg);
 
@@ -40,47 +36,22 @@ ros::Publisher pub_cmd_vel("/turtle1/cmd_vel", &cmd_vel_msg);
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+int key_signal;
+
 void print_key()
 {
     lcd.clear();
     lcd.setCursor(0,0);
-        lcd.print("k:");lcd.print(key_signal);
+    lcd.print("k:");lcd.print(key_signal);
 }
 
-void print_angle()
+void print_pose()
 {
     lcd.clear();
     lcd.setCursor(0,0);
-        lcd.print("as1:");lcd.print(alpha_1);lcd.print(" ");
-        lcd.print("as2:");lcd.print(alpha_2);
-
+    lcd.print("x:");lcd.print(x);
     lcd.setCursor(0,1);
-        lcd.print("ts:");lcd.print(teta);lcd.print(" ");
-}
-
-void printSignalError(int sig, int ref)
-{
-    lcd.clear();
-    lcd.setCursor(0,0);
-        lcd.print("c:");lcd.print(sig);lcd.print(" ");
-        lcd.print("r:");lcd.print(ref);
-
-    lcd.setCursor(0,1);
-        lcd.print("e:");lcd.print(ref-sig);lcd.print(" ");
-}
-
-
-// -----------------------------------------------------------------------------
-const int stepsPerRevolution = 200;
-
-int step_action(int sig, int ref)
-{
-    if (sig < ref)
-        sig++;
-    else if (sig > ref)
-        sig--;
-
-    return sig;
+    lcd.print("y:");lcd.print(y);
 }
 
 // -----------------------------------------------------------------------------
@@ -110,42 +81,53 @@ void setup()
     // INIT ROS NODE  ===============================
     nh.initNode();
 
-    nh.subscribe(sub_alpha_1);
-    nh.subscribe(sub_alpha_2);
-    nh.subscribe(sub_teta);
-
+    nh.subscribe(sub_msg_pose);
     nh.advertise(pub_cmd_vel);
 }
 
 void loop()
 {
-    alpha_1 = step_action(alpha_1, ref_alpha_1);
-    alpha_2 = step_action(alpha_2, ref_alpha_2);
-    teta = step_action(teta, ref_teta);
-
     nh.spinOnce();
 
     key_signal = analogRead(0);
 
-    if(key_signal<128)
+    if(key_signal<100)
     {
-        cmd_vel_msg.linear.x = 0.1;
+        cmd_vel_msg.linear.x = 0.3;
         cmd_vel_msg.linear.y = 0.0;
         cmd_vel_msg.linear.z = 0.0;
         cmd_vel_msg.angular.x = 0.0;
         cmd_vel_msg.angular.y = 0.0;
         cmd_vel_msg.angular.z = -0.9;
     }
-    else if(key_signal>500 && key_signal<800)
+    else if(key_signal>100 && key_signal<400)
     {
-        cmd_vel_msg.linear.x = 0.1;
+        cmd_vel_msg.linear.x = 0.3;
+        cmd_vel_msg.linear.y = 0.0;
+        cmd_vel_msg.linear.z = 0.0;
+        cmd_vel_msg.angular.x = 0.0;
+        cmd_vel_msg.angular.y = 0.0;
+        cmd_vel_msg.angular.z = 0.0;
+    }
+    else if(key_signal>400 && key_signal<600)
+    {
+        cmd_vel_msg.linear.x = -0.3;
+        cmd_vel_msg.linear.y = 0.0;
+        cmd_vel_msg.linear.z = 0.0;
+        cmd_vel_msg.angular.x = 0.0;
+        cmd_vel_msg.angular.y = 0.0;
+        cmd_vel_msg.angular.z = 0.0;
+    }
+    else if(key_signal>600 && key_signal<900)
+    {
+        cmd_vel_msg.linear.x = 0.3;
         cmd_vel_msg.linear.y = 0.0;
         cmd_vel_msg.linear.z = 0.0;
         cmd_vel_msg.angular.x = 0.0;
         cmd_vel_msg.angular.y = 0.0;
         cmd_vel_msg.angular.z = 0.9;
     }
-    else if(key_signal>1000)
+    else if(key_signal>900)
     {
         cmd_vel_msg.linear.x = 0.0;
         cmd_vel_msg.linear.y = 0.0;
@@ -157,7 +139,7 @@ void loop()
 
     pub_cmd_vel.publish(&cmd_vel_msg);
 
-    print_key();
+    print_pose();
 
-    delay(5);
+    delay(100);
 }
